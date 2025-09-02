0068ee4278908cc0b1f5d64b25daa487e75d92a7
Msingh-ML <ai.manishkrsingh@gmail.com>
2025-09-02 10:46:56 +0530

    added comment on each code

--- 1stprogram.py
+++ 1stprogram.py
@@ -1,62 +1,68 @@
-# a = int(input("enter a number: "))
-# b = int(input("enter another number: "))
-# c = a + b
-# print("The sum of", a, "and", b, "is ", c)
+# This is my first python program
+a = int(input("enter a number: "))
+b = int(input("enter another number: "))
+c = a + b
+print("The sum of", a, "and", b, "is ", c)
 
-# side = int(input("Enter the length of a side of the square: "))
-# area = side * side
-# print("The area of the square is", area)
-# Uncomment the lines below to run the original code
+# Arithmetic operations
+side = int(input("Enter the length of a side of the square: "))
+area = side * side
+print("The area of the square is", area)
 
-# a = float(input("Enter a number: "))
-# b = float(input("Enter another number: "))
-# avg = (a + b) / 2
-# print("The average of", a, "and", b, "is", avg)
+# Average of two numbers
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+avg = (a + b) / 2
+print("The average of", a, "and", b, "is", avg)
 
-# a = int(input("Enter a number: "))
-# b = int(input("Enter another number: "))
-# bool = a >= b
-# print("The first number is greater than or eaqual to the second:", bool)
+# Comparison operators
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+bool = a >= b
+print("The first number is greater than or eaqual to the second:", bool)
 
 # Strings
+# length of a string
+name = input("Enter first your name: ")
+print(len(name))
 
-# name = input("Enter first your name: ")
-# print(len(name))
-
-# a = input("any sentence with $")
-# print("no of $ in this sentence is ", a.count("$"))
+# counting specific character in a string
+a = input("any sentence with $")
+print("no of $ in this sentence is ", a.count("$"))
 
 # conditional statement
+# grading system
+marks = int(input("Enter your marks : "))
+if(marks >= 90):
+    print("Grade A")
+elif(90 > marks >= 80):
+    print("Grade B")
+elif(80 > marks >= 70):
+    print("Grade C")
+else:
+    print("Grade D") 
 
-# marks = int(input("Enter your marks : "))
+# even or odd
+num = int(input("enter number "))
+if(num % 2 == 0):
+    print("number is even")
+else:
+    print("number is odd")
 
-# if(marks >= 90):
-#     print("Grade A")
-# elif(90 > marks >= 80):
-#     print("Grade B")
-# elif(80 > marks >= 70):
-#     print("Grade C")
-# else:
-#     print("Grade D") 
+# biggest of three numbers
+a = int(input("enter 1st number "))
+b = int(input("enter 2nd number "))
+c = int(input("enter 3rd number "))
+if(a >= b and a >= c):
+    big = a
+elif(b >= c):
+    big = b
+else:
+    big = c
 
-# num = int(input("enter number "))
-# if(num % 2 == 0):
-#     print("number is even")
-# else:
-#     print("number is odd")
+print("biggest number is ", big)
 
-# a = int(input("enter 1st number "))
-# b = int(input("enter 2nd number "))
-# c = int(input("enter 3rd number "))
-# if(a >= b and a >= c):
-#     big = a
-# elif(b >= c):
-#     big = b
-# else:
-#     big = c
-
-# print("biggest number is ", big)
-
+# multiple of 7
 num = int(input("Enter a number "))
 if(num % 7 == 0):
     print(num,"is a multiple of 7")
